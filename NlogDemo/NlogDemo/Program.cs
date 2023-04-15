4a88a2b5545ed7573bda3c1c3eaeb0cfffc68e5d
Princekotadiya007 <princuuu77@gmail.com>
2023-04-15 18:16:39 +0530

    CRUD opreation

--- NlogDemo/NlogDemo/Program.cs
+++ NlogDemo/NlogDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,61 @@ namespace NlogDemo
     {
         static void Main(string[] args)
         {
-            Sum sum = new Sum();
-            sum.DoSum(10, 20);
+            //Sum sum = new Sum();
+            //sum.DoSum(10, 20);
+
+            // Create a new ArrayList
+            ArrayList list = new ArrayList();
+
+            // Add some items to the list
+            list.Add("Apple");
+            list.Add("Banana");
+            list.Add("Cherry");
+            list.Add("Durian");
+
+            // Print the items in the list
+            Console.WriteLine("Items in the list:");
+            PrintList(list);
+
+            // Update an item in the list
+            list[2] = "Grape";
+
+            // Print the items in the list again
+            Console.WriteLine("\nItems in the list after updating an item:");
+            PrintList(list);
+
+            // Remove an item from the list
+            list.RemoveAt(1);
+
+            // Print the items in the list again
+            Console.WriteLine("\nItems in the list after removing an item:");
+            PrintList(list);
+
+            // Insert an item into the list
+            list.Insert(0, "Fig");
+
+            // Print the items in the list again
+            Console.WriteLine("\nItems in the list after inserting an item:");
+            PrintList(list);
+
+            // Clear all items from the list
+            list.Clear();
+
+            // Print the items in the list again
+            Console.WriteLine("\nItems in the list after clearing all items:");
+            PrintList(list);
+
+            Console.ReadKey();
+        }
+
+        // Helper method to print the items in the list
+        static void PrintList(ArrayList list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
